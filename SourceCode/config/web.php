89b522aa6a1ebfa96a96c9d2bc15a93217908f72
Tho Tran <phucthotran@gmail.com>
2014-11-10 14:50:55 +0700

    Config Debug module for working on local

--- SourceCode/config/web.php
+++ SourceCode/config/web.php
@@ -45,7 +45,7 @@ $config = [
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['modules']['debug'] = ['class' => 'yii\debug\Module', 'allowedIPs' => ['127.0.0.1', '192.168.1.*']];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
