19181200d5e524c407957da12e09173bb779d1db
Tho Tran <phucthotran@gmail.com>
2014-11-16 11:14:18 +0700

    Change 'app\models\ContactForm' to 'app\models\PageForm'

--- SourceCode/modules/admin/views/page/edit.php
+++ SourceCode/modules/admin/views/page/edit.php
@@ -27,7 +27,7 @@ use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\ContactForm */
+/* @var $model app\models\PageForm */
 	
 $this->title = 'Sửa Trang | Quốc Bảo - Control Panel';
 
--- SourceCode/modules/admin/views/page/new.php
+++ SourceCode/modules/admin/views/page/new.php
@@ -23,7 +23,7 @@ use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\ContactForm */
+/* @var $model app\models\PageForm */
 	
 $this->title = 'Thêm Trang | Quốc Bảo - Control Panel';
 
