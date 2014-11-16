12192abf5781233bdd93d95a8fc7d123b508c61a
Tho Tran <phucthotran@gmail.com>
2014-11-16 18:11:21 +0700

    Preview map for 'Map' page

--- SourceCode/modules/admin/views/map/index.php
+++ SourceCode/modules/admin/views/map/index.php
@@ -1,12 +1,41 @@
 <?php
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use app\models\Setting;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 
 $this->title = 'Bản Đồ';
+$this->registerJsFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', ['position' => \yii\web\View::POS_END]);
+$this->registerJsFile('http://maps.google.com/maps/api/js?sensor=false&libraries=geometry&v=3.7', ['position' => \yii\web\View::POS_END]);
+$this->registerJsFile('/web/js/maplace-0.1.3.min.js', ['position' => \yii\web\View::POS_END]);
+?>
 
+<?php 
+$maplaceScript = <<<EOT
+	new Maplace({
+			locations: [{ lat: {lat}, lon: {long}, zoom: 16 }],
+			controls_on_map: false,
+			show_infowindow: false,
+			map_options: {
+				scrollwheel: false
+			}
+	}).Load();
+EOT;
+
+if( Setting::findOne(['name' => 'map_lat']) != null ) {
+	$mapLat = strval( Setting::findOne(['name' => 'map_lat'])->value );
+}
+
+if( Setting::findOne(['name' => 'map_long']) != null ) {
+	$mapLong = strval( Setting::findOne(['name' => 'map_long'])->value );
+}
+
+$maplaceScript = str_replace('{lat}', $mapLat, $maplaceScript);
+$maplaceScript = str_replace('{long}', $mapLong, $maplaceScript);
+
+$this->registerJs($maplaceScript, \yii\web\View::POS_READY, 'maplace');
 ?>
 
 <?php if(Yii::$app->session->hasFlash('ChangeMapSuccess')): ?>
@@ -20,6 +49,8 @@ $this->title = 'Bản Đồ';
 
 <div class="panel panel-default">
 	<div class="panel-body">
+		<div style="height: 400px; margin-bottom: 30px;" id="gmap"></div>
+	
 		<?php $form = ActiveForm::begin(['id' => 'map-form']) ?>
 		<?= $form->field($model, 'lat') ?>
 		<?= $form->field($model, 'long') ?>
