e6d819470ba6342516662ac5ec3f9386a218508a
Tho Tran <phucthotran@gmail.com>
2014-11-17 10:55:49 +0700

    Rewrite SQL code

--- SQL/cuakinhsgdev_db.sql
+++ SQL/cuakinhsgdev_db.sql
@@ -1,164 +1,57 @@
--- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
---
--- Host: localhost    Database: cuakinhsgdev_db
--- ------------------------------------------------------
--- Server version	5.1.73
+CREATE TABLE announcements
+(
+id int NOT NULL AUTO_INCREMENT,
+title tinytext NULL,
+modeId tinyint(1) DEFAULT 0,
+publish tinyint(1) DEFAULT 1,
+content tinytext NOT NULL,
+PRIMARY KEY (id)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE admins
+(
+id int NOT NULL AUTO_INCREMENT,
+username varchar(45) NOT NULL,
+password varchar(45) NOT NULL,
+PRIMARY KEY (id)
+);
 
---
--- Table structure for table `admins`
---
+CREATE TABLE pages
+(
+id int NOT NULL AUTO_INCREMENT,
+title tinytext NOT NULL,
+url tinytext NOT NULL,
+keywords tinytext NOT NULL,
+publish tinyint(1) DEFAULT 1,
+content blob,
+PRIMARY KEY (id)
+);
 
-DROP TABLE IF EXISTS `admins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `admins` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE settings
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(45) NOT NULL,
+value text DEFAULT NULL,
+PRIMARY KEY (id)
+);
 
---
--- Dumping data for table `admins`
---
+-- INSERT DEFAULT DATA
 
-LOCK TABLES `admins` WRITE;
-/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `announcement`
---
-
-DROP TABLE IF EXISTS `announcement`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `announcement` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) NOT NULL,
-  `mode_id` tinyint(4) NOT NULL,
-  `publish` tinyint(4) DEFAULT '1',
-  `content` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX (mode_id),
-  
-  FOREIGN KEY (mode_id) 
-  	REFERENCES announcement_mode(id)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `announcement`
---
-
-LOCK TABLES `announcement` WRITE;
-/*!40000 ALTER TABLE `announcement` DISABLE KEYS */;
-/*!40000 ALTER TABLE `announcement` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `announcement_mode`
---
-
-DROP TABLE IF EXISTS `announcement_mode`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `announcement_mode` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `value` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `announcement_mode`
---
-
-LOCK TABLES `announcement_mode` WRITE;
-/*!40000 ALTER TABLE `announcement_mode` DISABLE KEYS */;
-/*!40000 ALTER TABLE `announcement_mode` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `pages`
---
-
-DROP TABLE IF EXISTS `pages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `pages` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `url` varchar(45) NOT NULL,
-  `keywords` varchar(100) NOT NULL,
-  `publish` tinyint(4) DEFAULT NULL,
-  `content` blob NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pages`
---
-
-LOCK TABLES `pages` WRITE;
-/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `settings`
---
-
-DROP TABLE IF EXISTS `settings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `settings` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `value` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `settings`
---
-
-LOCK TABLES `settings` WRITE;
-/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping events for database 'cuakinhsgdev_db'
---
-
---
--- Dumping routines for database 'cuakinhsgdev_db'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-10-27  9:39:40
+INSERT INTO settings (name, value) VALUES ('general_maintenance_enable', '0');
+INSERT INTO settings (name) VALUES ('general_maintenance_message');
+INSERT INTO settings (name, value) VALUES ('general_breadcrumb_enable', '1');
+INSERT INTO settings (name) VALUES ('general_web_name');
+INSERT INTO settings (name) VALUES ('general_web_title');
+INSERT INTO settings (name) VALUES ('general_corp_name');
+INSERT INTO settings (name) VALUES ('general_corp_email');
+INSERT INTO settings (name) VALUES ('general_corp_phone');
+INSERT INTO settings (name) VALUES ('map_lat');
+INSERT INTO settings (name) VALUES ('map_long');
+INSERT INTO settings (name) VALUES ('announcement_enable');
+INSERT INTO settings (name, value) VALUES ('widget_enable', '1');
+INSERT INTO settings (name) VALUES ('widget_1_title');
+INSERT INTO settings (name) VALUES ('widget_1_text');
+INSERT INTO settings (name) VALUES ('widget_2_title');
+INSERT INTO settings (name) VALUES ('widget_2_text');
+INSERT INTO settings (name) VALUES ('navbar_items');
+INSERT INTO settings (name) VALUES ('homepage_id');
