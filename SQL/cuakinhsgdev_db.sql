772435c5818139853a13fd901f4f3b42ec1a9317
Tho Tran <phucthotran@gmail.com>
2014-11-18 11:24:52 +0700

    Add new column for 'admins' and improve default 'data insert' scripts

--- SQL/cuakinhsgdev_db.sql
+++ SQL/cuakinhsgdev_db.sql
@@ -12,7 +12,9 @@ CREATE TABLE admins
 (
 id int NOT NULL AUTO_INCREMENT,
 username varchar(45) NOT NULL,
-password varchar(45) NOT NULL,
+password varchar(100) NOT NULL,
+authKey varchar(100) NOT NULL,
+accessToken varchar(100) NOT NULL,
 PRIMARY KEY (id)
 );
 
@@ -37,22 +39,24 @@ PRIMARY KEY (id)
 
 -- INSERT DEFAULT DATA
 
+INSERT INTO admins (username, password, authKey, accessToken) VALUES ('admin', '$2y$13$1yxF8LyB4WO4YARE0pKf8eLmpw0cU/pM4Dhw3rvQ5RBTEl6xxOElK', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', '44fd5b3d6f3f5cd03c865f9cd710e2c59d8ac839');
+
 INSERT INTO settings (name, value) VALUES ('general_maintenance_enable', '0');
-INSERT INTO settings (name) VALUES ('general_maintenance_message');
+INSERT INTO settings (name, value) VALUES ('general_maintenance_message', 'Website Underconstruction');
 INSERT INTO settings (name, value) VALUES ('general_breadcrumb_enable', '1');
-INSERT INTO settings (name) VALUES ('general_web_name');
-INSERT INTO settings (name) VALUES ('general_web_title');
-INSERT INTO settings (name) VALUES ('general_corp_name');
-INSERT INTO settings (name) VALUES ('general_corp_address');
-INSERT INTO settings (name) VALUES ('general_corp_email');
-INSERT INTO settings (name) VALUES ('general_corp_phone');
-INSERT INTO settings (name) VALUES ('map_lat');
-INSERT INTO settings (name) VALUES ('map_long');
+INSERT INTO settings (name, value) VALUES ('general_web_name', 'Tho Tran Coder');
+INSERT INTO settings (name, value) VALUES ('general_web_title', 'Tho Tran Coder Demo');
+INSERT INTO settings (name, value) VALUES ('general_corp_name', 'Tho Tran Coder');
+INSERT INTO settings (name, value) VALUES ('general_corp_address', 'Dak Mil, Dak Nong');
+INSERT INTO settings (name, value) VALUES ('general_corp_email', 'phucthotran@gmail.com');
+INSERT INTO settings (name, value) VALUES ('general_corp_phone', '01655973646');
+INSERT INTO settings (name, value) VALUES ('map_lat', '12.4667181');
+INSERT INTO settings (name, value) VALUES ('map_long', '107.6757788');
 INSERT INTO settings (name, value) VALUES ('announcement_enable', '1');
 INSERT INTO settings (name, value) VALUES ('widget_enable', '1');
-INSERT INTO settings (name) VALUES ('widget_1_title');
-INSERT INTO settings (name) VALUES ('widget_1_text');
-INSERT INTO settings (name) VALUES ('widget_2_title');
-INSERT INTO settings (name) VALUES ('widget_2_text');
+INSERT INTO settings (name, value) VALUES ('widget_1_title', 'Tho Tran Coder');
+INSERT INTO settings (name, value) VALUES ('widget_1_text', '<p><span class="glyphicon glyphicon-user"></span> <strong>NAME: </strong>Tran Phuc Tho</p><p><span class="glyphicon glyphicon-briefcase"></span> <strong>JOB: </strong>Software/Web Developer</p>');
+INSERT INTO settings (name, value) VALUES ('widget_2_title', 'Keep In Touch');
+INSERT INTO settings (name, value) VALUES ('widget_2_text', '<p><span class="glyphicon glyphicon-home"></span> <strong>FACEBOOK </strong><a href="http://www.facebook.com/thotran.developer">Tho Tran Coder</a></p><p><span class="glyphicon glyphicon-inbox"></span> <strong>EMAIL </strong><a href="mailto:phucthotran@gmail.com">phucthotran@gmail.com</a></p><p><span class="glyphicon glyphicon-phone"></span> <strong>PHONE </strong>01655973646</p>');
 INSERT INTO settings (name) VALUES ('navbar_items');
 INSERT INTO settings (name) VALUES ('homepage_id');
